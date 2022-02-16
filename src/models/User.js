a67215ece605f103420b51415fa7e1bfba910092
yoontaeho <pln0302@yonsei.ac.kr>
2022-02-16 13:42:07 +0900

    Added User model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true, trim: true },
+  username: { type: String, required: true, trim: true },
+  password: { type: String, required: true },
+  avatarUrl: { type: String },
+  viewed: [
+    {
+      video: { type: mongoose.ObjectId, required: true, ref: "Video" },
+      time: { type: String, required: true },
+    },
+  ],
+  booked: [{ type: mongoose.ObjectId, ref: "Lecture" }],
+  liked: [{ type: mongoose.ObjectId, ref: "Lecture" }],
+  hated: [{ type: mongoose.ObjectId, ref: "Lecture" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
