dde609e65e7d3c23c8b481e6748c7a7849da40e6
yoontaeho <pln0302@yonsei.ac.kr>
2022-02-16 14:19:17 +0900

    Added lectureControllers

--- src/controllers/lectureController.js
+++ src/controllers/lectureController.js
@@ -6,7 +6,7 @@ import Topic from "../models/Topic";
 export const getLecturePreviews = async (req, res) => {
   try {
     const lectures = await Lecture.find({})
-      .select(["title", "thumbnailUrl", "topics"])
+      .select(`${process.env.LECTURE_PREVIEW_FIELDS}`)
       .populate({
         path: "topics",
         select: "name",
@@ -17,6 +17,58 @@ export const getLecturePreviews = async (req, res) => {
   }
 };
 
-export const getLectureDetail = (req, res) => {};
-export const getLecturesOfTopic = (req, res) => {};
-export const getLecturesOfInstructors = (req, res) => {};
+export const getLectureDetail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const lecture = await Lecture.findById(id)
+      .populate([
+        { path: "instructors", select: "name" },
+        { path: "topics", select: "name" },
+        { path: "videos", select: ["title", "description", "thumbnailUrl"] },
+      ])
+      .lean();
+    // error process
+    if (!lecture) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).json(lecture);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getLecturesOfTopic = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const topic = await Topic.findById(id)
+      .populate({
+        path: "lectures",
+        select: `${process.env.LECTURE_PREVIEW_FIELDS}`,
+      })
+      .lean();
+    // error process
+    if (!topic) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).json(topic);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getLecturesOfInstructors = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const instructor = await Instructor.findById(id).populate({
+      path: "lectures",
+      select: `${process.env.LECTURE_PREVIEW_FIELDS}`,
+    });
+    // error process
+    if (!instructor) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).json(instructor);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
